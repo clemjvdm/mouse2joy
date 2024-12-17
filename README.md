f5edc16ef7304ae5bee9ba5e7e770abf7cb63eda
clement <clement.jvdm@gmail.com>
2024-12-18 00:39:28 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+##### Mouse2Joy
+An ultra simple CLI tool for linux which enables you to convert mouse input to a virtual joystick. Only x-axis input is converted since this is intended for racing games. I recommend pairing this tool with a joystick/gamepad visualization tool to have a clear view of how far you are steering ;).
+
+#### Installation 
+
+#### Usage
+
+#### Lore
+I actually started working on a much more feature full keyboard and mouse to virtual gamepad tool (kbm-gamepad). But I didn't want to keep working on it as I wasn't sure it was going to be useful to anyone (it wasn't going to be for me).
+
+But I can imagine it being useful in some scenarios, and since the tool is close to being done if someone thinks it would be useful let me know and I can probably finish it. Until then, this tool fills just my use case and nothing more :).
+
+#### To-do
+To do's before release 1.0:
+- [ ] Better CLI
+- [ ] Allow different levels of logging
+- [ ] (maybe) A small GUI to indicate the current joystick position
+- [ ] (maybe) Configure joystick configuration through a .toml file
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,154 @@
-fn main() {
-    println!("Hello, world!");
+use evdev::{uinput::VirtualDeviceBuilder, uinput::VirtualDevice, AbsoluteAxisType, Device, EventType, InputEvent, InputEventKind, RelativeAxisType, UinputAbsSetup, AbsInfo, Key};
+use thiserror::Error;
+use std::fs;
+
+const VJOYSTICK_NAME: &str = "mouse2joy";
+
+// virtual joystick axis setup
+const RANGE: i32 = 1024;
+const FUZZ: i32 = 0;
+const FLAT: i32 = 0;
+const RESOLUTION: i32 = 1;
+const VALUE: i32 = 0; // initial position of joystick
+
+// virtual joystick buttons, won't be used but increase chances of joystick being recognized
+static KEYS: [Key; 14] = [
+    Key::BTN_EAST,
+    Key::BTN_SOUTH,
+    Key::BTN_NORTH,
+    Key::BTN_WEST,
+    Key::BTN_DPAD_UP,
+    Key::BTN_DPAD_DOWN,
+    Key::BTN_DPAD_LEFT,
+    Key::BTN_DPAD_RIGHT,
+    Key::BTN_SELECT,
+    Key::BTN_START,
+    Key::BTN_TL,
+    Key::BTN_TR,
+    Key::BTN_TL2,
+    Key::BTN_TR2,
+];
+
+#[derive(Error, Debug)]
+pub enum Mouse2JoyError {
+
+  #[error("Failed to find a mouse device")]
+  NoMouseError,
+
+  #[error("Failed to read a mouse input")]
+  FailedToReadInput,
+
+}
+
+
+fn main() -> Result<(), Mouse2JoyError> {
+
+  // find all input devices that can be used as a mouse
+  let mut mouse_devices: Vec<Device> = fs::read_dir("/dev/input")
+    .unwrap()
+    .filter_map(Result::ok)
+    .filter_map(|entry| entry.path().into_os_string().to_str().map(String::from))
+    .filter_map(|path| {
+      Device::open(&path)
+        .ok()
+        .filter(|device| device.supported_events().contains(EventType::RELATIVE))
+    })
+    .collect();
+
+  if mouse_devices.is_empty() {
+    return Err(Mouse2JoyError::NoMouseError)
+  }
+
+  // ask user which mouse to use
+  if !(mouse_devices.len() == 1) {
+    println!("Several mouses detected, please select one:");
+    for (i,mouse) in mouse_devices.iter().enumerate() {
+      println!("{}: {}", i+1, mouse.name().unwrap_or("Unknown Device"));
+    }
+  }
+  
+  let index = input_in_range(1, mouse_devices.len());
+  let mut mouse = mouse_devices.remove(index-1);
+  println!("[INFO]: Using \"{}\" as an input device.", mouse.name().unwrap_or("Unknown Device")); // TODO:
+                                                                                                  // improve
+                                                                                                  // logging
+
+  // ungrab unwanted mouse devices
+  for mut device in mouse_devices {
+    device.ungrab().unwrap_or_else(|e| eprintln!("[WARNING]: Failed to ungrab device: {}", e));
+  }
+
+  // set up virtual joystick
+  let axis_info = AbsInfo::new(VALUE, -RANGE/2, RANGE/2, FUZZ, FLAT, RESOLUTION);
+  let mut joystick = create_joystick(axis_info, VJOYSTICK_NAME).unwrap();
+  
+  // fetch events and send them through to virtual joystick
+  let min: i32 = -512; // TODO: Make configurable
+  let max: i32 = 512; // TODO: Make configurable
+  let mut mouse_x_pos: i32 = 0;
+  let mut joystick_x_pos: i32;
+  loop {
+    match mouse.fetch_events() {
+      Ok(events) => {
+        for ev in events { // TODO: Error handling
+          if ev.kind() == InputEventKind::RelAxis(RelativeAxisType::REL_X) {
+            mouse_x_pos += ev.value();
+            joystick_x_pos = mouse_x_pos;
+            if joystick_x_pos < min { joystick_x_pos = min }
+            if joystick_x_pos > max { joystick_x_pos = max }
+            let ev = InputEvent::new(EventType::ABSOLUTE, AbsoluteAxisType::ABS_X.0, joystick_x_pos);
+            joystick.emit(&[ev]).unwrap(); // TODO: Error handling
+            println!("[INFO]: Moved joystick position to {}", joystick_x_pos);
+          }
+        }
+      }
+      Err(e) => {
+        println!("[WARNING]: Failed to fetch mouse events: {}", e); // improve logging
+        continue;
+      }
+    }
+  }
+}
+
+fn create_joystick(abs_info: AbsInfo, name: &str) -> std::io::Result<VirtualDevice> {
+
+  let abs_x = UinputAbsSetup::new(AbsoluteAxisType::ABS_X, abs_info);
+  let abs_y = UinputAbsSetup::new(AbsoluteAxisType::ABS_Y, abs_info);
+
+  let mut keys = evdev::AttributeSet::new();
+  for button in KEYS {
+    keys.insert(button)
+  }
+
+  let joystick = VirtualDeviceBuilder::new()?
+    .name(name)
+    .with_absolute_axis(&abs_x)?
+    .with_absolute_axis(&abs_y)?
+    .with_keys(&keys)?
+    .build()?;
+
+  Ok(joystick)
+
+}
+
+// ask user for a usize input within a given range
+fn input_in_range(min: usize, max: usize) -> usize {
+  let mut input = String::new();
+  
+  loop {
+    input.clear();
+    std::io::stdin()
+      .read_line(&mut input)
+      .expect("Failed to read line");
+
+    match input.trim().parse::<usize>() {
+      Ok(index) if index >= min && index <= max => {
+        return index;
+      }
+      _ => {
+        println!("Invalid selection. Please enter a number between {} and {}", min, max);
+        continue;
+      }
+    }
+  }
 }
